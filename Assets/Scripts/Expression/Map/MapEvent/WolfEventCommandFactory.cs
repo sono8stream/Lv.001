90602a53db3e69e96085468ca365315c7b24e09c
SONO <sono.stream8580@gmail.com>
2022-03-20 22:46:13 +0900

    Refactor

--- Assets/Scripts/Expression/Map/MapEvent/WolfEventCommandFactory.cs
+++ Assets/Scripts/Expression/Map/MapEvent/WolfEventCommandFactory.cs
@@ -15,44 +15,6 @@ namespace Expression.Map.MapEvent
             this.startOffset = startOffset;
         }
 
-        public EventCommandBase Create2(out int nextOffset)
-        {
-            int currentOffset = startOffset;
-            int variableCount = reader.ReadByte(currentOffset, out currentOffset);
-            int commandType = reader.ReadInt(currentOffset, true, out currentOffset);
-            //Debug.Log(commandType.ToString("X8"));
-            EventCommandBase command = new EventCommandBase();
-
-            switch (commandType)
-            {
-                case 0x00000065:
-                    command = CreateShowTextCommand(currentOffset, out currentOffset);
-                    break;
-                case 0x00000067:
-                    CreateDebugTextCommand(currentOffset, out currentOffset);
-                    break;
-                case 0x00000066:
-                    command = CreateChoiceForkCommand(currentOffset, out currentOffset);
-                    break;
-                case 0x0000006F:
-                    CreateFlagForkByVariableCommand(currentOffset, out currentOffset, variableCount);
-                    break;
-                case 0x00000191:
-                    command = CreateForkBeginCommand(currentOffset, out currentOffset);
-                    break;
-                case 0x000000D2:
-                    CreateCallEventByIdCommand(currentOffset, out currentOffset);
-                    break;
-                default:
-                    CreateDefaultCommand(currentOffset, out currentOffset, variableCount);
-                    break;
-            }
-
-            nextOffset = currentOffset;
-            startOffset = currentOffset;
-            return command;
-        }
-
         public EventCommandBase Create(out int nextOffset)
         {
             int currentOffset = startOffset;
@@ -63,19 +25,19 @@ namespace Expression.Map.MapEvent
             switch (metaCommand.NumberArgs[0])
             {
                 case 0x00000065:
-                    command = CreateShowTextCommand2(metaCommand.StringArgs);
+                    command = CreateShowTextCommand(metaCommand.StringArgs);
                     break;
                 case 0x00000067:
                     // デバッグ文。処理なし
                     break;
                 case 0x00000066:
-                    command = CreateChoiceForkCommand2(metaCommand);
+                    command = CreateChoiceForkCommand(metaCommand);
                     break;
                 case 0x0000006F:
-                    CreateFlagForkByVariableCommand2(metaCommand);
+                    CreateFlagForkByVariableCommand(metaCommand);
                     break;
                 case 0x00000191:
-                    command = CreateForkBeginCommand2(metaCommand);
+                    command = CreateForkBeginCommand(metaCommand);
                     break;
                 case 0x000000D2:
                     CreateCallEventByIdCommand(currentOffset, out currentOffset);
@@ -127,54 +89,7 @@ namespace Expression.Map.MapEvent
             return new MetaEventCommand(numberVariables, stringVariables, indentDepth, footer);
         }
 
-        // すべてのコマンドで共通の設定
-        // ただし動作指定コマンドのみフッタの後に動作指定イベントが連続する
-        private EventCommandBase CreateDefaultCommand(int offset, out int nextOffset, int numberVariableCount)
-        {
-            int currentOffset = offset;
-
-            // 番号-1だけ読み取り
-            for (int i = 0; i < numberVariableCount - 1; i++)
-            {
-                int val = reader.ReadInt(currentOffset, true, out currentOffset);
-                //Debug.Log($"数値変数{i}：{val}");
-            }
-
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-            for (int i = 0; i < stringVariableCount; i++)
-            {
-                string text = reader.ReadString(currentOffset, out currentOffset);
-                //Debug.Log($"文字列変数{i}：{text}");
-            }
-
-            int footer = reader.ReadByte(currentOffset, out currentOffset);
-            // フッタの値が1の場合、動作指定コマンドなので移動ルート読み取りを行う
-            if (footer == 1)
-            {
-                ReadEventMoveRoute(currentOffset, out currentOffset);
-            }
-
-            nextOffset = currentOffset;
-            return null;
-        }
-
-        private EventCommandBase CreateShowTextCommand(int offset, out int nextOffset)
-        {
-            int currentOffset = offset;
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-            string text = reader.ReadString(currentOffset, out currentOffset);
-            Debug.Log($"文章表示：{text}");
-            // フッタはスキップ
-            reader.ReadByte(currentOffset, out currentOffset);
-
-            nextOffset = currentOffset;
-            return new MessageCommand(text);
-        }
-
-        private EventCommandBase CreateShowTextCommand2(string[] stringVariables)
+        private EventCommandBase CreateShowTextCommand(string[] stringVariables)
         {
             string text = stringVariables[0];
             Debug.Log($"文章表示：{text}");
@@ -182,45 +97,7 @@ namespace Expression.Map.MapEvent
             return new MessageCommand(text);
         }
 
-        private EventCommandBase CreateDebugTextCommand(int offset, out int nextOffset)
-        {
-            int currentOffset = offset;
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-            string text = reader.ReadString(currentOffset, out currentOffset);
-            Debug.Log($"デバッグ文：{text}");
-            // フッタはスキップ
-            reader.ReadByte(currentOffset, out currentOffset);
-
-            nextOffset = currentOffset;
-            return null;
-        }
-
-        private EventCommandBase CreateChoiceForkCommand(int offset, out int nextOffset)
-        {
-            int currentOffset = offset;
-
-            int forkParams = reader.ReadInt(currentOffset, true, out currentOffset);
-            int forkCount = forkParams % (1 << 4);
-            int otherForkOption = forkParams / (1 << 8) % (1 << 8);
-
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-            string[] choiceStrings = new string[stringVariableCount];
-            for (int i = 0; i < stringVariableCount; i++)
-            {
-                choiceStrings[i] = reader.ReadString(currentOffset, out currentOffset);
-                Debug.Log($"選択肢{i}：{choiceStrings[i]}");
-            }
-
-            // フッタはスキップ
-            reader.ReadByte(currentOffset, out currentOffset);
-
-            nextOffset = currentOffset;
-            return new ChoiceForkCommand(indentDepth,choiceStrings);
-        }
-
-        private EventCommandBase CreateChoiceForkCommand2(MetaEventCommand metaCommand)
+        private EventCommandBase CreateChoiceForkCommand(MetaEventCommand metaCommand)
         {
             int forkParams = metaCommand.NumberArgs[1];
             int forkCount = forkParams % (1 << 4);
@@ -234,32 +111,7 @@ namespace Expression.Map.MapEvent
             return new ChoiceForkCommand(metaCommand.IndentDepth, metaCommand.StringArgs);
         }
 
-        private EventCommandBase CreateFlagForkByVariableCommand(int offset, out int nextOffset, int numberVariableCount)
-        {
-            int currentOffset = offset;
-
-            int forkParams = reader.ReadInt(currentOffset, true, out currentOffset);
-            int forkCount = forkParams % (1 << 4);
-            int flagCount = (numberVariableCount - 2) / 3;
-            Debug.Log($"条件数：{flagCount}、分岐数：{forkCount}");
-            for (int i = 0; i < flagCount; i++)
-            {
-                int flagLeft = reader.ReadInt(currentOffset, true, out currentOffset);
-                int flagRight = reader.ReadInt(currentOffset, true, out currentOffset);
-                int rightAndCompareParams = reader.ReadInt(currentOffset, true, out currentOffset);
-            }
-
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-
-            // フッタはスキップ
-            reader.ReadByte(currentOffset, out currentOffset);
-
-            nextOffset = currentOffset;
-            return null;
-        }
-
-        private EventCommandBase CreateFlagForkByVariableCommand2(MetaEventCommand metaCommand)
+        private EventCommandBase CreateFlagForkByVariableCommand(MetaEventCommand metaCommand)
         {
             int forkParams = metaCommand.NumberArgs[1];
             int forkCount = forkParams % (1 << 4);
@@ -275,30 +127,7 @@ namespace Expression.Map.MapEvent
             return null;
         }
 
-        /// <summary>
-        /// 分岐始点を示すコマンドを生成
-        /// 選択肢、条件分岐共通
-        /// </summary>
-        /// <param name="offset"></param>
-        /// <param name="nextOffset"></param>
-        /// <returns></returns>
-        private EventCommandBase CreateForkBeginCommand(int offset, out int nextOffset)
-        {
-            int currentOffset = offset;
-            int forkNumber = reader.ReadInt(currentOffset, true, out currentOffset);
-            int indentDepth = reader.ReadByte(currentOffset, out currentOffset);
-            int stringVariableCount = reader.ReadByte(currentOffset, out currentOffset);
-
-            Debug.Log($"分岐始点{forkNumber}");
-
-            // フッタはスキップ
-            reader.ReadByte(currentOffset, out currentOffset);
-
-            nextOffset = currentOffset;
-            return new ForkBeginCommand(indentDepth, forkNumber);
-        }
-
-        private EventCommandBase CreateForkBeginCommand2(MetaEventCommand metaCommand)
+        private EventCommandBase CreateForkBeginCommand(MetaEventCommand metaCommand)
         {
             int forkNumber = metaCommand.NumberArgs[1];
 
