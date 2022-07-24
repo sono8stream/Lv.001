11a33f3d28a2c3ffb5cb5d3de569910b495ec384
SONO <sono.stream8580@gmail.com>
2022-07-24 22:24:21 +0900

    Fix ui namespace

--- Assets/Resources/Scripts/Town/PlayerController.cs
+++ Assets/Resources/Scripts/Town/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UI.Map;
 
 public class PlayerController : MonoBehaviour
 {
--- Assets/Scripts/UI/Map/Map.cs
+++ Assets/Scripts/UI/Map/Map.cs
@@ -3,86 +3,89 @@ using System.Collections;
 using System.Collections.Generic;
 using Expression;
 
-public class Map : MonoBehaviour
+namespace UI.Map
 {
-    [SerializeField]
-    private int mapIndex;
-
-    [SerializeField]
-    private GameObject eventObjectOrigin;
-
-    private Expression.Map.MapData mapData;
-
-    // Use this for initialization
-    void Awake()
+    public class Map : MonoBehaviour
     {
-        Expression.Map.MapId mapId = new Expression.Map.MapId(mapIndex);
-        mapData = DI.DependencyInjector.It().MapDataRepository.Find(mapId);
+        [SerializeField]
+        private int mapIndex;
 
-        SpriteRenderer underRenderer = transform.Find("UnderSprite").GetComponent<SpriteRenderer>();
+        [SerializeField]
+        private GameObject eventObjectOrigin;
 
-        Sprite underSprite = Sprite.Create(mapData.UnderTexture, new Rect(0, 0, mapData.UnderTexture.width, mapData.UnderTexture.height), Vector2.zero, mapData.PixelPerUnit);
-        underSprite.texture.filterMode = FilterMode.Point;
-        underRenderer.sprite = underSprite;
+        private Expression.Map.MapData mapData;
 
-        SpriteRenderer upperRenderer = transform.Find("UpperSprite").GetComponent<SpriteRenderer>();
-
-        Sprite upperSprite = Sprite.Create(mapData.UpperTexture, new Rect(0, 0, mapData.UnderTexture.width, mapData.UnderTexture.height), Vector2.zero, mapData.PixelPerUnit);
-        upperSprite.texture.filterMode = FilterMode.Point;
-        upperRenderer.sprite = upperSprite;
-
-        GenerateEventObjects(mapData);
-
-        FpsFixer.FixFrameRate();
-    }
-
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    public Expression.Map.MovableInfo[,] GetMovableInfo()
-    {
-        if (mapData == null)
+        // Use this for initialization
+        void Awake()
         {
-            return null;
+            Expression.Map.MapId mapId = new Expression.Map.MapId(mapIndex);
+            mapData = DI.DependencyInjector.It().MapDataRepository.Find(mapId);
+
+            SpriteRenderer underRenderer = transform.Find("UnderSprite").GetComponent<SpriteRenderer>();
+
+            Sprite underSprite = Sprite.Create(mapData.UnderTexture, new Rect(0, 0, mapData.UnderTexture.width, mapData.UnderTexture.height), Vector2.zero, mapData.PixelPerUnit);
+            underSprite.texture.filterMode = FilterMode.Point;
+            underRenderer.sprite = underSprite;
+
+            SpriteRenderer upperRenderer = transform.Find("UpperSprite").GetComponent<SpriteRenderer>();
+
+            Sprite upperSprite = Sprite.Create(mapData.UpperTexture, new Rect(0, 0, mapData.UnderTexture.width, mapData.UnderTexture.height), Vector2.zero, mapData.PixelPerUnit);
+            upperSprite.texture.filterMode = FilterMode.Point;
+            upperRenderer.sprite = upperSprite;
+
+            GenerateEventObjects(mapData);
+
+            FpsFixer.FixFrameRate();
         }
-        else
+
+        void Start()
         {
-            return mapData.MovableGrid;
         }
-    }
 
-    private void GenerateEventObjects(Expression.Map.MapData mapData)
-    {
-        for (int i = 0; i < mapData.EventDataArray.Length; i++)
+        // Update is called once per frame
+        void Update()
         {
-            GameObject gameObject = Instantiate(eventObjectOrigin);
-            Vector2Int pos = new Vector2Int(mapData.EventDataArray[i].PosX, mapData.EventDataArray[i].PosY);
-            gameObject.transform.position = Util.Map.PositionConverter.GetUnityPos(pos, mapData.Height);
+        }
 
-            ActionProcessor eventObject = gameObject.GetComponent<ActionProcessor>();
-            if (eventObject)
+        public Expression.Map.MovableInfo[,] GetMovableInfo()
+        {
+            if (mapData == null)
             {
-                eventObject.SetEventData(mapData.EventDataArray[i]);
-            }
-
-            // この処理はカプセル化できるのでEventObjectクラスに委譲したほうがよさそう
-            Texture2D currentTexture = mapData.EventDataArray[i].PageData[0].GetCurrentTexture();
-            SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-            if (currentTexture == null)
-            {
-                spriteRenderer.sprite = null;
+                return null;
             }
             else
             {
-                Sprite sprite = Sprite.Create(currentTexture, new Rect(0, 0, currentTexture.width, currentTexture.height), new Vector2(0.5f, 0.5f), mapData.PixelPerUnit);
-                sprite.texture.filterMode = FilterMode.Point;
-                spriteRenderer.sprite = sprite;
+                return mapData.MovableGrid;
+            }
+        }
+
+        private void GenerateEventObjects(Expression.Map.MapData mapData)
+        {
+            for (int i = 0; i < mapData.EventDataArray.Length; i++)
+            {
+                GameObject gameObject = Instantiate(eventObjectOrigin);
+                Vector2Int pos = new Vector2Int(mapData.EventDataArray[i].PosX, mapData.EventDataArray[i].PosY);
+                gameObject.transform.position = Util.Map.PositionConverter.GetUnityPos(pos, mapData.Height);
+
+                ActionProcessor eventObject = gameObject.GetComponent<ActionProcessor>();
+                if (eventObject)
+                {
+                    eventObject.SetEventData(mapData.EventDataArray[i]);
+                }
+
+                // この処理はカプセル化できるのでEventObjectクラスに委譲したほうがよさそう
+                Texture2D currentTexture = mapData.EventDataArray[i].PageData[0].GetCurrentTexture();
+                SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+                if (currentTexture == null)
+                {
+                    spriteRenderer.sprite = null;
+                }
+                else
+                {
+                    Sprite sprite = Sprite.Create(currentTexture, new Rect(0, 0, currentTexture.width, currentTexture.height), new Vector2(0.5f, 0.5f), mapData.PixelPerUnit);
+                    sprite.texture.filterMode = FilterMode.Point;
+                    spriteRenderer.sprite = sprite;
+                }
             }
         }
     }
